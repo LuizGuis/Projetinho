2ca7319844d9099f7f8959710cacc86c3b0f52d8
Luiz Guilherme Barboza Chagas <113631898+LuizGuis@users.noreply.github.com>
2022-11-05 18:05:50 -0300

    Add files via upload

---
+++ app4.py
@@ -0,0 +1,14 @@
+#nome=input('Digite seu nome')
+#idade= int (input(f"Qual é a sua idade{nome}?"))
+
+#nascimento=2022 - idade
+
+#print(f'{nome} Voce nasceu em {nascimento}')
+#print(f'Seu nome é {nome} e sua idade é {idade}')
+
+num1=int(input('Digite um numero: '))
+num2=int(input('Digite um numero: '))
+
+resultado=num1+num2
+
+print(f"o resultado é:{resultado}")
